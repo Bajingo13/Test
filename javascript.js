b73be33541f51653aa243d2e34d7bb01888ac2b5
Bajingo13 <teesoii12@gmail.com>
2025-07-14 13:47:16 +0800

    added new forms

--- javascript.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-  document.getElementById("chartForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("chartForm").style.display = "none";
-}
