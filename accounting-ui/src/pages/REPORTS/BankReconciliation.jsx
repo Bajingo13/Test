3ad916d190e5a094bf881c623ca257a0a395e7c3
Bajingo13 <teesoii12@gmail.com>
2026-07-02 14:47:28 +0800

    no data inside BR and CIS

---
+++ accounting-ui/src/pages/REPORTS/BankReconciliation.jsx
@@ -0,0 +1,8 @@
+export default function BankReconciliation() {
+  return (
+    <div>
+      <h1>Bank Reconciliation</h1>
+      <p>Bank Reconciliation page coming soon.</p>
+    </div>
+  );
+}
---
+++ accounting-ui/src/pages/REPORTS/ComparativeIncomeStatement.jsx
@@ -0,0 +1,8 @@
+export default function ComparativeIncomeStatement() {
+  return (
+    <div>
+      <h1>Comparative Income Statement</h1>
+      <p>Comparative Income Statement page coming soon.</p>
+    </div>
+  );
+}
